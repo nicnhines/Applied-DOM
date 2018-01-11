2d286579e3afa91529bb58c09c9f39e084da911e
nick <nicnines3@gmail.com>
2018-01-10 15:14:05 -1000

    second button finished

--- js/app.js
+++ js/app.js
@@ -83,6 +83,11 @@
    * to set the context to the correct object
    */
 
+   btn2.addEventListener('click',() => {
+    setMyLightGreen.apply(btn2);
+   })
+   
+
 
   /*
    * Add a click event listener to btn3
